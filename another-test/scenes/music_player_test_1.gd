af22c6a4677e7eb78cf646fd2e7527c1f1dab3e0
facundo <juliabarrios782@gmail.com>
2026-01-10 19:47:16 -0300

    fixed: for loop skipping songs when trying to erase non-music files, i might be dumb

--- another-test/scenes/music_player_test_1.gd
+++ another-test/scenes/music_player_test_1.gd
@@ -5,10 +5,11 @@ extends Control
 @export var next : Button
 @export var music_player : AudioStreamPlayer
 @export var progress_bar : HSlider
+@export var check_folder : Button
 @export var fileDialog : FileDialog
 
 #TODO: make the path changable
-var path : String = "/home/clumpl/Música/Lateralis - OTXO Original Soundtrack/"
+var path : String
 var _progress : float = 0.0
 var songs_list : PackedStringArray
 var current_song : int = 0
@@ -17,6 +18,13 @@ var stop : bool = false
 
 func _ready() -> void:
 	fileDialog.dir_selected.connect(get_dir)
+	fileDialog.popup()
+	check_folder.pressed.connect(open_file_dialog)
+
+
+func get_dir(pathh : String) -> void: # doing all the stuff AFTER file is selected
+	path = pathh+"/"
+	_after_file_access()
 
 func _process(_delta: float) -> void:
 	if(music_player.playing && !stop):
@@ -25,23 +33,23 @@ func _process(_delta: float) -> void:
 		if(m_playback_time != null):
 			progress_bar.value = m_playback_time / m_time
 
-func _do_ready():
+func _after_file_access():
 	songs_list = DirAccess.open(path).get_files()
 	assert(DirAccess.get_open_error() == 0, "An Error ocurred while opening a folder")
-	
-	var first_song : String = path+songs_list[current_song]
-	for song in songs_list:
-		if( song.get_extension() != "wav" &&
-			song.get_extension() != "ogg" &&
-			song.get_extension() != "mp3"
-		  ):
+	# when i erase something from the list while iterating on it
+	# it could have unintended behaviour
+	for song in songs_list.duplicate():
+		if( song.get_extension() != "wav" && song.get_extension() != "ogg" 
+									 	  && song.get_extension() != "mp3"):
 			songs_list.erase(song)
 			print("deleted " + song)
 			continue
 		var l := Label.new()
 		l.text = song.get_basename()
 		container.add_child(l)
-	music_player.stream = AudioStreamOggVorbis.load_from_file(first_song)
+	
+	var first_song : String = path+songs_list[current_song]
+	load_music(first_song)
 	assert(music_player.stream != null, "music stream is not setted, no music found in this folder")
 	
 	progress_bar.drag_started.connect(stop_it)
@@ -71,7 +79,7 @@ func next_song():
 	current_song = (current_song+1) % songs_list.size()
 	_change_color(Color(0.0, 255.014, 0.0))
 	var song_now = path + songs_list[current_song]
-	music_player.stream = AudioStreamOggVorbis.load_from_file(song_now)
+	load_music(song_now)
 	music_player.play()
 	progress_bar.value = music_player.get_playback_position()
 
@@ -84,26 +92,39 @@ func last_song():
 	if(current_song < 0):
 		current_song = songs_list.size()-1
 	var song_now = path + songs_list[current_song]
-	music_player.stream = AudioStreamOggVorbis.load_from_file(song_now)
+	load_music(song_now)
 	music_player.play()
 	progress_bar.value = music_player.get_playback_position()
 
-
+# TODO: change this to Container logic
 func _change_color(color : Color):
 	var a := container.get_children()
 	if(a[current_song] is Label):
 		var ll : Label = a[current_song]
 		ll.modulate = color
-		
+
+# TODO: change this to HSlider logic
 func stop_it():
 	stop = true
 	play_pause_music()
 
-func slider(_holo) -> void:
+func slider(_holo):
 	stop = false
 	music_player.play((music_player.stream.get_length()/100)*progress_bar.value)
 	_change_color(Color(0.0, 255.014, 0.0))
 
-func get_dir(pathh : String) -> void:
-	path = pathh+"/"
-	_do_ready()
+func load_music(holo : String): # func needs the full path or extension
+	var song_now =  path + songs_list[current_song]
+	match holo.get_extension():
+		"wav":
+			music_player.stream = AudioStreamWAV.load_from_file(song_now)
+		"ogg":
+			music_player.stream = AudioStreamOggVorbis.load_from_file(song_now)
+		"mp3":
+			music_player.stream = AudioStreamMP3.load_from_file(song_now)
+		_:
+			assert(false, "file type " + holo.get_extension() + " unknown")
+	assert(music_player.stream != null, "song selected has not been queried correctly")
+
+func open_file_dialog():
+	fileDialog.popup()
