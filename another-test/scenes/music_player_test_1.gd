54a6ba9304b527696d7fe22235e225c8800f9577
facundo <juliabarrios782@gmail.com>
2026-01-10 18:07:24 -0300

    added slider to music player

--- another-test/scenes/music_player_test_1.gd
+++ another-test/scenes/music_player_test_1.gd
@@ -4,6 +4,7 @@ extends Control
 @export var pause : Button
 @export var next : Button
 @export var music_player : AudioStreamPlayer
+@export var progress_bar : HSlider
 
 #TODO: make the path changable
 var path : String = "/home/clumpl/Música/Lateralis - OTXO Original Soundtrack/"
@@ -11,8 +12,10 @@ var _progress : float = 0.0
 var songs_list : PackedStringArray
 var current_song : int = 0
 
+var stop : bool = false
 
 func _ready() -> void:
+	
 	songs_list = DirAccess.open(path).get_files()
 	assert(DirAccess.get_open_error() == 0, "An Error ocurred while opening a folder")
 	
@@ -26,22 +29,35 @@ func _ready() -> void:
 			songs_list.erase(song)
 	music_player.stream = AudioStreamOggVorbis.load_from_file(first_song)
 	
+	progress_bar.drag_started.connect(stop_it)
+	progress_bar.drag_ended.connect(slider)
+	
 	pause.pressed.connect(play_pause_music)
 	next.pressed.connect(next_song)
 	last.pressed.connect(last_song)
 
+func _process(_delta: float) -> void:
+	if(music_player.playing && !stop):
+		var m_playback_time = music_player.get_playback_position()
+		var m_time = music_player.stream.get_length()/100
+		if(m_playback_time != null):
+			progress_bar.value = m_playback_time / m_time
 
 #TODO: add this to AudioStreamPlayer's script
 func play_pause_music() -> void:
+	var m_playback_time = music_player.get_playback_position()
+	var m_time = music_player.stream.get_length()/100
 	if(music_player.playing):
-		_progress = music_player.get_playback_position()
+		_progress = m_playback_time
 		_change_color(Color(255.014, 0.0, 0.0))
+		progress_bar.value = m_playback_time / m_time
 		music_player.stop()
 	else:
 		music_player.play(_progress)
 		_change_color(Color(0.0, 255.014, 0.0))
 
 func next_song():
+	progress_bar.value = 0
 	music_player.stop()
 	_change_color(Color.WHITE)
 	current_song = (current_song+1) % songs_list.size()
@@ -49,8 +65,10 @@ func next_song():
 	var song_now = path + songs_list[current_song]
 	music_player.stream = AudioStreamOggVorbis.load_from_file(song_now)
 	music_player.play()
+	progress_bar.value = music_player.get_playback_position()
 
 func last_song():
+	progress_bar.value = 0
 	music_player.stop()
 	_change_color(Color.WHITE)
 	current_song = (current_song-1) % songs_list.size()
@@ -60,6 +78,7 @@ func last_song():
 	var song_now = path + songs_list[current_song]
 	music_player.stream = AudioStreamOggVorbis.load_from_file(song_now)
 	music_player.play()
+	progress_bar.value = music_player.get_playback_position()
 
 
 func _change_color(color : Color):
@@ -67,3 +86,12 @@ func _change_color(color : Color):
 	if(a[current_song] is Label):
 		var ll : Label = a[current_song]
 		ll.modulate = color
+		
+func stop_it():
+	stop = true
+	play_pause_music()
+
+func slider(_holo) -> void:
+	stop = false
+	music_player.play((music_player.stream.get_length()/100)*progress_bar.value)
+	
